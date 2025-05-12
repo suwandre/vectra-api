ef42e0ec8ac699ab97bb048dcf9756e54f5282dd
Suwandre <suwandresukijat@gmail.com>
2025-05-13 00:03:12 +0700

    rf: socket address port replaced to 8000

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> anyhow::Result<()> {
         .layer(cors);
 
     // Bind and serve
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("🚀 Server running on http://{}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
